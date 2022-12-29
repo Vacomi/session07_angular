57de0550ae2db19fe1776e1c79517bfb12944b2b
niscely <romeronisely@gmail.com>
2022-12-29 17:49:06 -0500

    agregando navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule
