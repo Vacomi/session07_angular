8417e2a80a02fa6d3edf7ec78ec1129600cf2568
Michael Vargas <vacomi.corp@gmail.com>
2022-12-29 17:24:46 -0500

    pagina 2 finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina2Component,
   ],
   imports: [
     BrowserModule
