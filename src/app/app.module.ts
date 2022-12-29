3783269bf25fee31872d7db54d4829b7aa8bcdab
ChristianVilchezA <christian.servtec@gmail.com>
2022-12-29 17:41:17 -0500

    cambios en page3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Pagina3Component } from './pagina3/pagina3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina3Component
   ],
   imports: [
     BrowserModule
